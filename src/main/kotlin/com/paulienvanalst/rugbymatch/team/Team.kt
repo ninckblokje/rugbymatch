41266a2ca6ee2cbc45e17aba37776a9de609da48
ninckblokje <jeroen.ninckblok@gmail.com>
2019-01-22 19:47:41 +0100

    exercise 1

--- src/main/kotlin/com/paulienvanalst/rugbymatch/team/Team.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/team/Team.kt
@@ -6,26 +6,27 @@ data class Team (val players: List<Player>, val name: TeamName) {
      * A team has enough players when it has at least 15 players
      */
     val hasEnoughPlayers : Boolean
-        get() = throw NotImplementedException()
+        get() = players.size >= 15
 
     /**
      * A team has substitutes when it has more than 15 players
      */
     val hasAnySubstitutes : Boolean
-        get() = throw NotImplementedException()
+        get() = players.any { player -> !player.isStarting }
 
     /**
      * A team has enough starting players when there are at least 15 players
      * wearing back numbers 1 until 15
      */
     val hasEnoughStartingPlayers : Boolean
-        get() = throw NotImplementedException()
+        get() = players.filter { player -> player.isStarting }.size == 15
 
     /**
      * The captain, when present, should always where back number 7
      */
     fun captainBackNumber(): Int? {
-        throw NotImplementedException()
+        //return players.first { it.backNumber == 7 }.backNumber
+        return players.find { it.backNumber == 7 }!!.backNumber
     }
 
     /**
@@ -33,7 +34,7 @@ data class Team (val players: List<Player>, val name: TeamName) {
      * the captain is wearing the first back number we can find among the starting players.
      */
     fun replacingCaptainBackNumber(): Int? {
-        throw NotImplementedException()
+        return players.find { player -> player.isStarting }?.backNumber
     }
 
 
